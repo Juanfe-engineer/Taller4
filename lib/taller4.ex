e9d6ed1822c99af3ef2c88053d9d9949aefca348
juange-engineer <juanf.ibarral@uqvirtual.edu.co>
2026-05-02 20:35:19 -0500

    Inicializar Proyecto

---
+++ lib/taller4.ex
@@ -0,0 +1,18 @@
+defmodule Taller4 do
+  @moduledoc """
+  Documentation for `Taller4`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Taller4.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/taller4_test.exs
@@ -0,0 +1,8 @@
+defmodule Taller4Test do
+  use ExUnit.Case
+  doctest Taller4
+
+  test "greets the world" do
+    assert Taller4.hello() == :world
+  end
+end
