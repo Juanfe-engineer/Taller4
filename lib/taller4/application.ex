30e6e288605dd8ffee736dd9bae75ff6eb2fb108
juange-engineer <juanf.ibarral@uqvirtual.edu.co>
2026-05-05 11:41:21 -0500

    Conectar menú principal con gimnasio e inventario en Application

--- lib/taller4/application.ex
+++ lib/taller4/application.ex
@@ -1,20 +1,43 @@
 defmodule Taller4.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   @impl true
   def start(_type, _args) do
-    children = [
-      # Starts a worker by calling: Taller4.Worker.start_link(arg)
-      # {Taller4.Worker, arg}
-    ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
+    children = []
     opts = [strategy: :one_for_one, name: Taller4.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, pid} = Supervisor.start_link(children, opts)
+
+    menu_principal()
+
+    {:ok, pid}
+  end
+
+  defp menu_principal do
+    IO.puts("""
+    \n
+         SISTEMA DE GESTIÓN
+             TALLER 4
+
+    1. Gestión de Gimnasio
+    2. Sistema de Inventario
+    0. Salir
+    """)
+
+    case IO.gets("Elige una opción: ") |> String.trim() do
+      "1" ->
+        Taller4.Gimnasio.Menu.iniciar()
+        menu_principal()
+
+      "2" ->
+        Taller4.Inventario.Menu.iniciar()
+        menu_principal()
+
+      "0" ->
+        IO.puts("\n ¡Hasta luego!")
+
+      _ ->
+        IO.puts(" Opción inválida, intenta de nuevo.")
+        menu_principal()
+    end
   end
 end
